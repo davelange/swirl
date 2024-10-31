bf59dd57cfa36295995db9020d9139c197a2c179
David Lange <lange@finiam.com>
2024-10-31 17:28:40 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ export default {
   server: {
     host: true, // Open to local network and display URL
   },
-  build: {
-    outDir: "../dist", // Output in the dist/ folder
-    emptyOutDir: true, // Empty the folder first
-    sourcemap: true, // Add sourcemap
-  },
   plugins: [
     restart({ restart: ["./public/**"] }), // Resatart server on static file change
     glsl(), // Handle shader files
